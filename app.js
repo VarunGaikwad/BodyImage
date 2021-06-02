a7eaa247648acc678058232f3498d1ec70834594
Varun <gaikwadvarun23@gmail.com>
2021-06-02 14:46:41 +0530

    Yellow Color Added 👁️

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function onBodyImageSelection() {
         element.addEventListener("click", function (event) {
             const oElement = event.target.parentElement;
             if (oElement.getAttribute("class") !== "nose" && oElement.getAttribute("class") !== "mouth") {
-                oElement.style.filter = oElement.style.filter === "none" ? "grayscale(1)" : "none";
+                oElement.style.filter = oElement.style.filter === "grayscale(1)" ? "invert(93%) sepia(24%) saturate(6360%) hue-rotate(5deg) brightness(111%) contrast(108%)" : "grayscale(1)";
             }
         }.bind(self));
     }.bind(self));
